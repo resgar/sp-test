8e657e519ffd537c4f87539bca95eaa9589ebbbf
Sajad Rastegar <rastegar.sajjad@gmail.com>
2022-08-23 12:43:33 +0200

    Show analysis with correct input file

--- parser.rb
+++ parser.rb
@@ -6,3 +6,28 @@ abort 'The file parameter is required.' if ARGV.empty?
 file_path = ARGV[0]
 
 abort 'No such file.' unless File.exist?(file_path)
+
+def analyze(data)
+  data.map do |path, ips|
+    ips = yield ips if block_given?
+    { path:, ips: ips.count }
+  end
+end
+
+file = File.open(file_path)
+
+analysis = Hash.new { |h, k| h[k] = [] }
+
+file.each do |line|
+  path, ip = line.split(' ')
+  analysis[path] << ip
+end
+
+page_views = analyze(analysis)
+uniq_page_views = analyze(analysis, &:uniq)
+
+puts 'Pages views:'
+puts page_views
+
+puts 'Unique page views:'
+puts uniq_page_views
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,19 +1,35 @@
-spec/parser_spec.rb# frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'spec_helper'
 
 describe 'parser' do
-  subject { ->(file_path) { `./parser.rb #{file_path}` } }
+  subject { system "./parser.rb #{file_path}" }
 
   context 'when no file specifed' do
+    let(:file_path) { nil }
+
     specify do
-      expect { subject.call(nil) }.to output("The file parameter is required.\n").to_stderr_from_any_process
+      expect { subject }.to output(/The file parameter is required/).to_stderr_from_any_process
     end
   end
 
   context "when the file doesn't exist" do
+    let(:file_path) { 'unknown_file' }
+
     specify do
-      expect { subject.call('unknown_file') }.to output("No such file.\n").to_stderr_from_any_process
+      expect { subject }.to output(/No such file/).to_stderr_from_any_process
+    end
+  end
+
+  context 'when the file exists' do
+    let(:file_path) { 'spec/fixtures/files/webserver.log' }
+
+    specify do
+      expect { subject }.to output(/Pages views/).to_stdout_from_any_process
+    end
+
+    specify do
+      expect { subject }.to output(/Unique page views/).to_stdout_from_any_process
     end
   end
 end
